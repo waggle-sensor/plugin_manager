368acc5a43f89f5e0df2546187ea2162f336ab37
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-01-06 16:34:10 -0600

    cleanup

--- scripts/send.py
+++ scripts/send.py
@@ -1,4 +1,4 @@
-import time, socket, sys
+import time, socket, sys, logging
 
 #gets the IP address for the nodecontroller
 with open('/etc/waggle/NCIP','r') as file_:
@@ -6,6 +6,10 @@ with open('/etc/waggle/NCIP','r') as file_:
     
 with open('/etc/waggle/hostname','r') as file_:
     HOSTNAME = file_.read().strip()
+
+
+HOST = NCIP #sets to NodeController IP
+PORT = 9090 #port for push_server
     
 def send(msg):
     
@@ -17,25 +21,31 @@ def send(msg):
         
     """
 
-#TODO May want to add guestnode message robustness. Currently, if node controller is currently unavailable, all guest node messages are lost. Can add a loop or
-#TODO Can add robustness by making this a daemon process that plugins can connect to using unix sockets. Send process puts messages in a queue to add robustness. 
-#TODO Send process can pull messages and send to NC if connected or hold on to the messages until NC connects. 
-
-    HOST = NCIP #sets to NodeController IP
-    PORT = 9090 #port for push_server
-
-    try:
+    #TODO May want to add guestnode message robustness. Currently, if node controller is currently unavailable, all guest node messages are lost. Can add a loop or
+    #TODO Can add robustness by making this a daemon process that plugins can connect to using unix sockets. Send process puts messages in a queue to add robustness. 
+    #TODO Send process can pull messages and send to NC if connected or hold on to the messages until NC connects. 
+   
+    
+    s = None
+    try: 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try: 
-            s.connect((HOST,PORT))
-            s.send(msg)
-            s.close() #closes each time a message is sent.
-        #print 'Connection closed...'
-        except Exception as e: 
-            print e
-            time.sleep(1)
-            #print 'Unable to connect...'
-    except Exception as e:
-        print e
-        print 'Connection disrupted...'
+    except Exception as e: 
+        logger.error("Could not create socket to %s:%d : %s" % (HOST, PORT, str(e)))
+        raise    
+        
+    try: 
+        s.connect((HOST,PORT))
+    except Exception as e: 
+        logger.error("Could not connect to %s:%d : %s" % (HOST, PORT, str(e)))
+        raise
+        
+    try:
+        s.send(msg)
+    except Exception as e: 
+        logger.error("Could not send message to %s:%d : %s" % (HOST, PORT, str(e)))
+        raise
+   
+    logger.debug("Did send message to nodecontroller.")
+    
+    if s:
         s.close()
