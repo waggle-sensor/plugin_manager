4bf7b62ddd00b4580e7194352a7c8c0397ddc770
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-02-04 10:30:16 -0600

    aplha version of new build script

--- scripts/build_waggle_image.py
+++ scripts/build_waggle_image.py
@@ -4,17 +4,17 @@ from subprocess import call, check_call
 import os.path
 
 
-DIR="/root"
+data_directory="/root"
 
-REPORT_FILE="/root/report.txt"
+report_file="/root/report.txt"
 
 
-images={'odroid-xu3' : "ubuntu-14.04lts-server-odroid-xu3-20150725.img"}
+base_images= {'odroid-xu3' : "ubuntu-14.04lts-server-odroid-xu3-20150725.img"}
 
 
 
 '''
-Will throw exception on error.
+Will throw exception on execution error.
 '''
 def run_command(cmd):
     print "execute: %s" % (cmd) 
@@ -49,7 +49,7 @@ if not os.path.isfile('waggle_first_boot.sh') :
     sys.exit(1)
     
 
-SCRIPT_DIR=os.getcwd()
+#SCRIPT_data_directory=os.getcwd()
 
 
 if not call('hash partprobe &> /dev/null'):
@@ -70,54 +70,54 @@ if odroid_model_raw == "ODROIDXU":
 elif odroid_model_raw  == "ODROIDC":
     print "Detected device: %s" % (odroid_model)
     odroid_model="odroid-c1"
-else
-  print "Could not detect ODROID model. (%s)" % (odroid_model)
-  sys.exit(1)
-fi
+else:
+    print "Could not detect ODROID model. (%s)" % (odroid_model)
+    sys.exit(1)
 
 
 
 
-DATE=get_output('date +"%Y%m%d"') 
-NEW_IMAGE_PREFIX="%s/waggle-guestnode-%s-%s" % (DIR, odroid_model, DATE) 
-NEW_IMAGE="%s.img" % (NEW_IMAGE_PREFIX)
 
-NEW_IMAGE_B="%s_B.img" % (NEW_IMAGE_PREFIX)
+date_today=get_output('date +"%Y%m%d"') 
+new_image_prefix="%s/waggle-guestnode-%s-%s" % (data_directory, odroid_model, date_today) 
+new_image="%s.img" % (new_image_prefix)
+
+new_image_b="%s_B.img" % (new_image_prefix)
 
 
-os.chdir(DIR)
+os.chdir(data_directory)
 
 try:
-    IMAGE = images[odroid_model]
+    base_image = base_images[odroid_model]
 except:
     print "image %s not found" % (odroid_model)
     sys.exit(1)
 
-image_xz = IMAGE + '.xz'
+base_image_xz = base_image + '.xz'
 
-if not os.path.isfile(image_xz):
-  run_command('wget http://www.mcs.anl.gov/research/projects/waggle/downloads/'+ image_xz)
+if not os.path.isfile(base_image_xz):
+    run_command('wget http://www.mcs.anl.gov/research/projects/waggle/downloads/'+ base_image_xz)
   
 
-if not os.path.isfile(IMAGE):
-    run_command('unxz --keep '+image_xz)
+if not os.path.isfile(base_image):
+    run_command('unxz --keep '+base_image_xz)
     
 
-os.remove(NEW_IMAGE)
+os.remove(new_image)
 
-shutil.copyfile(IMAGE, NEW_IMAGE)
+shutil.copyfile(base_image, new_image)
 
 
 
 # get partition start position
-#fdisk -lu ${IMAGE}
-START_BLOCK=int(get_output("fdisk -lu %s | grep '%s2' | awk '{print $2}'" % (NEW_IMAGE, NEW_IMAGE)))
+#fdisk -lu ${base_image}
+start_block=int(get_output("fdisk -lu {0} | grep '{0}2' | awk '{print $2}'".format(new_image))
 
-START_POS=START_BLOCK*512  #get_output('echo "%s*512" | bc' % (START_BLOCK)) 
+START_POS=start_block*512  #get_output('echo "%s*512" | bc' % (start_block)) 
 
 
 # create loop device for disk and for root partition
-run_command('losetup /dev/loop0 ' + NEW_IMAGE)
+run_command('losetup /dev/loop0 ' + new_image)
 run_command('losetup -o %s /dev/loop1 /dev/loop0' % (str(START_POS)))
 
 
@@ -136,7 +136,8 @@ run_command('mount -o bind /sys  %s/sys' % (mount_point))
 ###  Script for chroot execution ###
 ###                              ###
 #${mount_point}/root/build_gn_image.sh
-build_guestnode_image='''#!/bin/bash
+build_guestnode_image='''\
+#!/bin/bash
 set -x
 set -e
 
@@ -186,7 +187,7 @@ rm -f /etc/network/interfaces.d/*
 rm -f /etc/udev/rules.d/70-persistent-net.rules 
 
 
-'''.format(REPORT_FILE)
+'''.format(report_file)
 
 
 
@@ -202,13 +203,14 @@ run_command('chroot %s /bin/bash /root/build_gn_image.sh' % (mount_point))
 # After changeroot
 #
 
-os.remove(REPORT_FILE)
-shutil.copyfile(mount_point+REPORT_FILE, NEW_IMAGE+'.report.txt')
+os.remove(report_file)
+shutil.copyfile(mount_point+report_file, new_image+'.report.txt')
 
 
 
 #set static IP
-guest_node_etc_network_interfaces_d = '''# interfaces(5) file used by ifup(8) and ifdown(8)
+guest_node_etc_network_interfaces_d = '''\
+# interfaces(5) file used by ifup(8) and ifdown(8)
 # Include files from /etc/network/interfaces.d:
 source-directory /etc/network/interfaces.d
 
@@ -230,82 +232,82 @@ write_file(mount_point+'/etc/network/interfaces', guest_node_etc_network_interfa
 
 
 
-OLD_PARTITION_SIZE_KB=int(get_output('df -BK --output=size /dev/loop1 | tail -n 1 | grep -o "[0-9]\+"'))
+old_partition_size_kb=int(get_output('df -BK --output=size /dev/loop1 | tail -n 1 | grep -o "[0-9]\+"'))
 
 run_command('umount /mnt/newimage/{proc,dev,sys,}')
 
 
 
-ESTIMATED_FS_SIZE_BLOCKS=int(get_output('resize2fs -P /dev/loop1 | grep -o "[0-9]*"') )
+estimated_fs_size_blocks=int(get_output('resize2fs -P /dev/loop1 | grep -o "[0-9]*"') )
 
-BLOCK_SIZE=int(get_output('blockdev --getbsz /dev/loop1'))
+block_size=int(get_output('blockdev --getbsz /dev/loop1'))
 
-ESTIMATED_FS_SIZE_KB = ESTIMATED_FS_SIZE_BLOCKS*BLOCK_SIZE/1024
+estimated_fs_size_kb = estimated_fs_size_blocks*block_size/1024
 
 
 # add 500MB
-NEW_PARTITION_SIZE_KB = ESTIMATED_FS_SIZE_KB + (1024*500)
+new_partition_size_kb = estimated_fs_size_kb + (1024*500)
 
 
 # add 100MB
-NEW_FS_SIZE_KB = ESTIMATED_FS_SIZE_KB + (1024*100)
+new_fs_size_kb = estimated_fs_size_kb + (1024*100)
 
 # verify partition:
 run_command('e2fsck -f -y /dev/loop1')
 
 
 
-SECTOR_SIZE=int(run_command('fdisk -lu {0} | grep "Sector size" | grep -o ": [0-9]*" | grep -o "[0-9]*"'.format(NEW_IMAGE)))
+sector_size=int(run_command('fdisk -lu {0} | grep "Sector size" | grep -o ": [0-9]*" | grep -o "[0-9]*"'.format(new_image)))
 
 
-FRONT_SIZE_KB = SECTOR_SIZE * START_BLOCK/ 1024
+front_size_kb = sector_size * start_block/ 1024
 
-if NEW_PARTITION_SIZE_KB < OLD_PARTITION_SIZE_KB: 
+if new_partition_size_kb < old_partition_size_kb: 
 
-  print "NEW_PARTITION_SIZE_KB is smaller than OLD_PARTITION_SIZE_KB"
+    print "new_partition_size_kb is smaller than old_partition_size_kb"
 
-  # shrink filesystem (that does not shrink the partition!)
-  run_command('resize2fs -p /dev/loop1 %sK' % (NEW_FS_SIZE_KB))
+    # shrink filesystem (that does not shrink the partition!)
+    run_command('resize2fs -p /dev/loop1 %sK' % (new_fs_size_kb))
 
 
-  run_command('partprobe  /dev/loop1')
+    run_command('partprobe  /dev/loop1')
 
-  time.sleep(3)
+    time.sleep(3)
 
-  ### fdisk (shrink partition)
-  # fdisk: (d)elete partition 2 ; (c)reate new partiton 2 ; specify start posirion and size of new partiton
+    ### fdisk (shrink partition)
+    # fdisk: (d)elete partition 2 ; (c)reate new partiton 2 ; specify start posirion and size of new partiton
   
-  run_command('echo -e "d\n2\nn\np\n2\n%d\n+%dK\nw\n" | fdisk %s' % (START_BLOCK, NEW_PARTITION_SIZE_KB, NEW_IMAGE))
+    run_command('echo -e "d\n2\nn\np\n2\n%d\n+%dK\nw\n" | fdisk %s' % (start_block, new_partition_size_kb, new_image))
   
 
 
-  run_command('partprobe /dev/loop1')
+    run_command('partprobe /dev/loop1')
 
-  #set +e
-  #resize2fs /dev/loop1
-  #set -e
+    #set +e
+    #resize2fs /dev/loop1
+    #set -e
 
-  # does not show the new size
-  #fdisk -lu ${NEW_IMAGE}
+    # does not show the new size
+    #fdisk -lu ${new_image}
 
-  # shows the new size (-b for bytes)
-  #partx --show /dev/loop1 (fails)
+    # shows the new size (-b for bytes)
+    #partx --show /dev/loop1 (fails)
 
-  time.sleep(3)
+    time.sleep(3)
 
-  run_command('e2fsck -n -f /dev/loop1')
+    run_command('e2fsck -n -f /dev/loop1')
 
-  #e2fsck_ok=1
-  #set +e
-  #while [ ${e2fsck_ok} != "0" ] ; do
-  #  e2fsck -f /dev/loop1
-  #  e2fsck_ok=$?
-  #  sleep 2
-  #done
-  #set -e
+    #e2fsck_ok=1
+    #set +e
+    #while [ ${e2fsck_ok} != "0" ] ; do
+    #  e2fsck -f /dev/loop1
+    #  e2fsck_ok=$?
+    #  sleep 2
+    #done
+    #set -e
 
 else:
-  print "NEW_PARTITION_SIZE_KB is NOT smaller than OLD_PARTITION_SIZE_KB"
+    print "new_partition_size_kb is NOT smaller than old_partition_size_kb"
 
 
 
@@ -316,39 +318,39 @@ run_command('losetup -d /dev/loop0')
 
 # add size of boot partition
 
-COMBINED_SIZE_KB = NEW_PARTITION_SIZE_KB+FRONT_SIZE_KB
-COMBINED_SIZE_BYTES = (NEW_PARTITION_SIZE_KB + FRONT_SIZE_KB) * 1024
+combined_size_kb = new_partition_size_kb+front_size_kb
+combined_size_bytes = (new_partition_size_kb + front_size_kb) * 1024
 
 # from kb to mb
-BLOCKS_TO_WRITE = COMBINED_SIZE_KB/1024
+blocks_to_write = combined_size_kb/1024
 
 
 
-run_command('pv -per --width 80 --size %d -f %s | dd bs=1M iflag=fullblock count=%d | xz -1 --stdout - > %s.xz_part' % (COMBINED_SIZE_BYTES, NEW_IMAGE, BLOCKS_TO_WRITE, NEW_IMAGE))
+run_command('pv -per --width 80 --size %d -f %s | dd bs=1M iflag=fullblock count=%d | xz -1 --stdout - > %s.xz_part' % (combined_size_bytes, new_image, blocks_to_write, new_image))
 
 
-os.rename(NEW_IMAGE+'.xz_part',  NEW_IMAGE+'.xz')
+os.rename(new_image+'.xz_part',  new_image+'.xz')
 
 
 
 
-if [ -e ${DIR}/waggle-id_rsa ] ; then
-  md5sum $(basename ${NEW_IMAGE}.xz) > ${NEW_IMAGE}.xz.md5sum 
-  scp -o "StrictHostKeyChecking no" -v -i ${DIR}/waggle-id_rsa ${NEW_IMAGE}.xz ${NEW_IMAGE}.xz.md5sum waggle@terra.mcs.anl.gov:/mcs/www.mcs.anl.gov/research/projects/waggle/downloads/unstable
+if os.path.isfile( data_directory+ '/waggle-id_rsa'):
+    scp_target = 'waggle@terra.mcs.anl.gov:/mcs/www.mcs.anl.gov/research/projects/waggle/downloads/unstable'
+    run_command('md5sum $(basename {0}.xz) > {0}.xz.md5sum'.format(new_image) ) 
+    run_command('scp -o "StrictHostKeyChecking no" -v -i {0}/waggle-id_rsa {1}.xz {1}.xz.md5sum {2}'.format(data_directory, new_image, scp_target))
   
-  if [ -e ${NEW_IMAGE_B}.xz ] ; then
-    # upload second image with different UUID's
-    md5sum $(basename ${NEW_IMAGE_B}.xz) > ${NEW_IMAGE_B}.xz.md5sum
-    scp -o "StrictHostKeyChecking no" -v -i ${DIR}/waggle-id_rsa ${NEW_IMAGE_B}.xz ${NEW_IMAGE_B}.xz.md5sum waggle@terra.mcs.anl.gov:/mcs/www.mcs.anl.gov/research/projects/waggle/downloads/unstable
-  fi
+    if os.path.isfile( new_image_b+'.xz'):
+        # upload second image with different UUID's
+        run_command( 'md5sum $(basename {0}.xz) > {0}.xz.md5sum'.format(new_image_b))
+        run_command('scp -o "StrictHostKeyChecking no" -v -i {0}/waggle-id_rsa {1}.xz {1}.xz.md5sum {2}'.format(data_directory, new_image_b, scp_target))
+
   
+    if os.path.isfile( new_image+'.report.txt'): 
+        run_command('scp -o "StrictHostKeyChecking no" -v -i {0}/waggle-id_rsa {1}.report.txt {2}'.format(data_directory, new_image,scp_target))
+      
   
-  if [ -e ${NEW_IMAGE}.report.txt ] ; then 
-    scp -o "StrictHostKeyChecking no" -v -i ${DIR}/waggle-id_rsa ${NEW_IMAGE}.report.txt waggle@terra.mcs.anl.gov:/mcs/www.mcs.anl.gov/research/projects/waggle/downloads/unstable
-  fi
+    if os.path.isfile( new_image+'.build_log.txt'): 
+        run_command('scp -o "StrictHostKeyChecking no" -v -i {0}/waggle-id_rsa {1}.build_log.txt {2}'.format(data_directory, new_image,scp_target))
+      
   
-  if [ -e ${NEW_IMAGE}.build_log.txt ] ; then 
-    scp -o "StrictHostKeyChecking no" -v -i ${DIR}/waggle-id_rsa ${NEW_IMAGE}.build_log.txt waggle@terra.mcs.anl.gov:/mcs/www.mcs.anl.gov/research/projects/waggle/downloads/unstable
-  fi
-  
-fi
+
