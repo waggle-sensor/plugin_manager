4d284edb11f2e9f69a6bc76768f417c8c47c9448
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-02-09 10:53:28 -0600

    add socket

--- scripts/plugin_manager.py
+++ scripts/plugin_manager.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import multiprocessing, time, sys, re, os, logging
+import multiprocessing, time, sys, re, os, logging, socket, json
 import plugins 
 import run_plugins_multi
 from tabulate import tabulate
@@ -126,6 +126,9 @@ def on_blacklist(name):
 def on_whitelist(name):
     return (name in get_whitelist())
 
+
+
+
 #Lists available plugins, if they are active, and whether they are present on the whitelist and blacklist
 def list_plugins_full():
     headers = ["plugin", "instance", "active", "whitelist", "blacklist"]
@@ -151,12 +154,138 @@ def list_plugins_full():
         if (not active):
             #print 'Plugin', name, 'is inactive. Whitelist:', on_whitelist(name), '  |  Blacklist:', on_blacklist(name)
             table.append([name, "", False, on_whitelist(name), on_blacklist(name)])
-            
-    print 'System plugins:'
-    print tabulate(system_table, headers, tablefmt="fancy_grid")
     
-    print 'User plugins:'        
-    print tabulate(user_table, headers, tablefmt="fancy_grid")
+            
+    #client_sock.sendall('System plugins:'+"\n")
+    #client_sock.sendall(tabulate(system_table, headers, tablefmt="fancy_grid"))
+    #client_sock.sendall(str(system_table)+"\n")
+    #client_sock.sendall('User plugins:'+"\n")
+    #client_sock.sendall(str(user_table)+"\n")
+    
+    results={}
+    results['objects']=[]
+    
+    system_obj = {}
+    system_obj['type']='table'
+    system_obj['title']='System plugins'
+    system_obj['header']=headers
+    system_obj['data']=system_table
+    
+    
+    user_obj = {}
+    user_obj['type']='table'
+    user_obj['title']='User plugins'
+    user_obj['header']=headers
+    user_obj['data']=user_table
+    
+    results['tables'].append(system_obj)
+    results['tables'].append(user_obj)
+    
+    return json.dumps(results)
+    
+    
+    #client_sock.sendall(tabulate(user_table, headers, tablefmt="fancy_grid"))
+
+
+
+def do_command(command):
+    
+    if (command == "startall"):
+        start_all_valid()
+    elif (command == "killall" or command == "ka"):
+        plug.kill_all()
+    elif (command == "startwhite" or command == "sw"):
+        start_whitelist()
+    elif (command == "stopall"):
+        plug.stop_all()
+    elif (command == "pauseall"):
+        plug.pause_all()
+    elif (command == "unpauseall"):
+        plug.unpause_all()
+    elif (command == "whitelist"):
+        whitelist = read_file('plugins/whitelist.txt')
+        print "Whitelist:\n",whitelist
+    elif (command == "blacklist"):
+        blacklist = read_file('plugins/blacklist.txt')
+        print "Blacklist:\n",blacklist
+    elif (command == "infoall"):
+        plug.info_all()
+
+    #if the entry matches the name of a plugin, go to plugin menu
+    elif (command in plugins.__all__):
+        while True:
+            print '\nWould you like to "start", "stop", "pause", "unpause", or "kill" the plugin? \nYou can also "blacklist" or "whitelist" the plugin, or get "info" on it. \nType "back" to go back to the main menu.'
+            command2 = raw_input('Enter your command: ')
+            if (command2 == "start"):
+                if (on_blacklist(command)):
+                    print 'Cannot start plugin', command, 'because it is blacklisted.'
+                else: 
+                    plug.start_plugin(command)
+                break
+            elif (command2 == "kill" or command2 == "k"):
+                plug.kill_plugin(command)
+                break
+            elif (command2 == "stop"):
+                plug.stop_plugin(command)
+                break
+            elif (command2 == "pause" or command2 =="p"):
+                plug.pause_plugin(command)
+                break
+            elif (command2 == "unpause"):
+                plug.unpause_plugin(command)
+                break
+            elif (command2 == "pid"):
+                print 'Plugin',command+"'s PID:",plug.plugin_pid(command)
+                break
+            elif (command2 == "info"):
+                plug.plugin_info(command)
+                break
+            #elif (command2 == "suspend"):
+            #    plug.suspend_plugin(command)
+            #    break
+            #elif (command2 == "resume"):
+            #    plug.resume_plugin(command)
+            #    break
+
+            #Go to whitelist/blacklist process, choose whether to add or remove from list
+            elif (command2 == "whitelist" or command2 == "blacklist"):
+                print 'Would you like to add or remove from', command2 + '?'
+                command3 = raw_input('Enter your command (add/remove): ')
+                if (command3 == "add"):
+                    change = manip_list(command,command2,"add")
+                    if (change == 1):
+                        print command, command2+"ed."
+                    elif (change == 0):
+                        print command, "already on", command2 + "."
+                    break
+                if (command3 == "remove" or command3 == "rm"):
+                    change = manip_list(command,command2,"rm")
+                    if change:
+                        print command, "removed from", command2 + "."
+                    else:
+                        print command, "not on", command2 + "."
+                    break
+                else:
+                    print "I didn't understand your answer."
+
+            elif (command2 == "back"):
+                break
+            else:
+                print "I didn't understand your command \"%s\"." % (command2)
+
+    #stops plugins (and kills if there's a stop failure) and exits the program            
+    elif (command == "quit" or command == "q"):
+        if (not (plug.stop_all() == 0)):
+            plug.kill_all()
+        return
+    elif (command == "list" or command == "l"):
+        result_json = list_plugins_full()
+        client_sock.sendall(result_json+"\n")
+    elif (command == "help" or command == "h"):
+        help_dialogue()
+    else:
+        print "I didn't understand your command \"%s\". Type \"help\" or \"h\" to review commands." % (command)
+
 
 
 
@@ -188,112 +317,75 @@ Available commands:
 The following plugins are available.
 '''
 
-    list_plugins_full()
-    print 'To see this list again, type "list". \nIf you need to review the commands, type "help".'
+    #list_plugins_full(None)
 
-    try:
-        while True:
+
+    socket_file = '/tmp/plugin_manager'
+
+    if os.path.exists(socket_file): #checking for the file
+        os.remove(socket_file)
+
+
+    server_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+
+    server_sock.bind(socket_file)
+
+    # listen for clients
+    server_sock.listen(1)
+
+
+
+
+
+    while True:
         
-            command = raw_input('\nMain menu\nEnter your command: ')
-            print ''
-            if (command == "startall"):
-                start_all_valid()
-            elif (command == "killall" or command == "ka"):
-                plug.kill_all()
-            elif (command == "startwhite" or command == "sw"):
-                start_whitelist()
-            elif (command == "stopall"):
-                plug.stop_all()
-            elif (command == "pauseall"):
-                plug.pause_all()
-            elif (command == "unpauseall"):
-                plug.unpause_all()
-            elif (command == "whitelist"):
-                whitelist = read_file('plugins/whitelist.txt')
-                print "Whitelist:\n",whitelist
-            elif (command == "blacklist"):
-                blacklist = read_file('plugins/blacklist.txt')
-                print "Blacklist:\n",blacklist
-            elif (command == "infoall"):
-                plug.info_all()
+        logger.debug("waiting for data")
+        
+        try:
+            client_sock, address = server_sock.accept()
+        except KeyboardInterrupt:
+            logger.info("Shutdown requested...exiting")
+            self.stop()
+            sys.exit(0)
+        except Exception as e:
+            logger.info("server_sock.accept: "+str(e))
+            self.stop()
+            sys.exit(1)
 
-            #if the entry matches the name of a plugin, go to plugin menu
-            elif (command in plugins.__all__):
-                while True:
-                    print '\nWould you like to "start", "stop", "pause", "unpause", or "kill" the plugin? \nYou can also "blacklist" or "whitelist" the plugin, or get "info" on it. \nType "back" to go back to the main menu.'
-                    command2 = raw_input('Enter your command: ')
-                    if (command2 == "start"):
-                        if (on_blacklist(command)):
-                            print 'Cannot start plugin', command, 'because it is blacklisted.'
-                        else: 
-                            plug.start_plugin(command)
-                        break
-                    elif (command2 == "kill" or command2 == "k"):
-                        plug.kill_plugin(command)
-                        break
-                    elif (command2 == "stop"):
-                        plug.stop_plugin(command)
-                        break
-                    elif (command2 == "pause" or command2 =="p"):
-                        plug.pause_plugin(command)
-                        break
-                    elif (command2 == "unpause"):
-                        plug.unpause_plugin(command)
-                        break
-                    elif (command2 == "pid"):
-                        print 'Plugin',command+"'s PID:",plug.plugin_pid(command)
-                        break
-                    elif (command2 == "info"):
-                        plug.plugin_info(command)
-                        break
-                    #elif (command2 == "suspend"):
-                    #    plug.suspend_plugin(command)
-                    #    break
-                    #elif (command2 == "resume"):
-                    #    plug.resume_plugin(command)
-                    #    break
 
-                    #Go to whitelist/blacklist process, choose whether to add or remove from list
-                    elif (command2 == "whitelist" or command2 == "blacklist"):
-                        print 'Would you like to add or remove from', command2 + '?'
-                        command3 = raw_input('Enter your command (add/remove): ')
-                        if (command3 == "add"):
-                            change = manip_list(command,command2,"add")
-                            if (change == 1):
-                                print command, command2+"ed."
-                            elif (change == 0):
-                                print command, "already on", command2 + "."
-                            break
-                        if (command3 == "remove" or command3 == "rm"):
-                            change = manip_list(command,command2,"rm")
-                            if change:
-                                print command, "removed from", command2 + "."
-                            else:
-                                print command, "not on", command2 + "."
-                            break
-                        else:
-                            print "I didn't understand your answer."
+        try:
+            data = client_sock.recv(512) #arbitrary
+        
+        except KeyboardInterrupt, k:
+            logger.info("KeyboardInterrupt")
+            break
+        except Exceptiomn as a:
+            logger.error("client_sock.recv failed: %s" % (str(e) ))
+            break    
+        
+        client_sock.sendall('hi there')
+            
+        logger.debug("got data: %s" % (str(data)))
+        
+        command = str(data).rstrip()
+        result_json = do_command(command )
+        client_sock.sendall(result_json+"\n")
+        
+        
+    sys.exit(0)    
 
-                    elif (command2 == "back"):
-                        break
-                    else:
-                        print "I didn't understand your command."
+    #print 'To see this list again, type "list". \nIf you need to review the commands, type "help".'
 
-            #stops plugins (and kills if there's a stop failure) and exits the program            
-            elif (command == "quit" or command == "q"):
-                if (not (plug.stop_all() == 0)):
-                    plug.kill_all()
-                break
-            elif (command == "list" or command == "l"):
-                list_plugins_full()
-            elif (command == "help" or command == "h"):
-                help_dialogue()
-            else:
-                print "I didn't understand your command. Type \"help\" or \"h\" to review commands."
+    #try:
+    #    while True:
+        
+    #        command = raw_input('\nMain menu\nEnter your command: ')
             
             
-    except KeyboardInterrupt:
-        print "exiting..."
-    except Exception as e:
-        print "error: "+str(e)
+            
+    #except KeyboardInterrupt:
+    #    print "exiting..."
+    #except Exception as e:
+    #    print "error: "+str(e)
             
