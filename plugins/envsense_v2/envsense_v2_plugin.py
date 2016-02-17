7b61ab2e6d2c59947f279bd1637bd84690290977
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-02-17 15:16:11 -0600

    uses timestamp from message now

--- plugins/envsense_v2/envsense_v2_plugin.py
+++ plugins/envsense_v2/envsense_v2_plugin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 import coresense
-import datetime
 
 
 logger = logging.getLogger(__name__)
@@ -14,38 +13,39 @@ class register(object):
     plugin_version = '2'
 
     def __init__(self, name, man, mailbox_outgoing):
-        man[name] = 1
+        self.name = name
+        self.man = man
         self.outqueue = mailbox_outgoing
+
         self.run(name, man)
 
-    def send_values(self, sensor_name, sensor_values):
-        timestamp_utc = datetime.datetime.utcnow()
-        timestamp_date = timestamp_utc.date()
-        timestamp_epoch = int(float(timestamp_utc.strftime("%s.%f")) * 1000)
+    def run(self):
+        with coresense.create_connection('/dev/ttyACM0') as conn:
+            self.man[self.name] = 1
+            while self.man[self.name] != 0:
+                message = conn.recv()
+                logger.info(message)
+                self.handle_message(message)
 
-        message = [
-            str(timestamp_date),
+    def stop(self):
+        self.man[self.name] = 0
+
+    def handle_message(self, message):
+        for entry in message.entries:
+            self.handle_message_entry(entry)
+
+    def handle_message_entry(self, message, entry):
+        self.outqueue.put([
+            str(message.timestamp.date()),
             self.plugin_name,
             self.plugin_version,
             'default',
-            str(timestamp_epoch),
-            sensor_name,
+            str(message.timestamp),
+            entry.sensor,
             'meta.txt',
-            sensor_values,
-        ]
+            format_entry_values(entry),
+        ])
 
-        self.outqueue.put(message)
 
-    def run(self, name, man):
-        # device = '/dev/tty.usbmodem1421'
-        device = '/dev/ttyACM0'
-
-        with coresense.create_connection(device) as conn:
-            while man[name]:
-                message = conn.recv()
-                logger.info(message)
-
-                for entry in message.entries:
-                    formatted_values = ['{}:{}'.format(key, value)
-                                        for key, value in entry.values]
-                    self.send_values(entry.sensor, formatted_values)
+def format_entry_values(entry):
+    return ['{}:{}'.format(key, value) for key, value in entry.values]
