b99932587619824de1e5434445448bc892a64cb2
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-09-29 15:36:57 -0500

    added device argument

--- plugins/gps/gps_plugin.py
+++ plugins/gps/gps_plugin.py
@@ -3,6 +3,7 @@ import logging
 import pynmea2
 from serial import Serial
 import waggle.pipeline
+import sys
 
 
 logger = logging.getLogger(__name__)
@@ -11,38 +12,42 @@ logger.setLevel(logging.DEBUG)
 
 class GPSPluign(waggle.pipeline.Plugin):
 
-  plugin_name = 'gps'
-  plugin_version = '1'
+    plugin_name = 'gps'
+    plugin_version = '1'
 
-  def run(self):
-    device = '/dev/gps_module'
+    def run(self):
+        if len(sys.argv) == 1:
+            device = '/dev/gps_module'
+        else:
+            device = sys.argv[1]
 
-    serial = Serial(device, timeout=180)
+        serial = Serial(device, timeout=180)
 
-    while True:
-      line = serial.readline().decode()
-      if "$GPGGA" in line:
-        parsed_data = pynmea2.parse(line)
-        # check for empty values
-        try:
-          lat_degree = int(float(parsed_data.lat) / 100)
-          lat_minute = float(parsed_data.lat) - lat_degree*100
-          lon_degree = int(float(parsed_data.lon) / 100)
-          lon_minute = float(parsed_data.lon) - lon_degree*100
-          data_string = u'%s\u00b0%s\'%s,%s\u00b0%s\'%s,%s%s' % (lat_degree, lat_minute, parsed_data.lat_dir,
-                                                                 lon_degree, lon_minute, parsed_data.lon_dir,
-                                                                 parsed_data.altitude, parsed_data.altitude_units.lower())
+        while True:
+            line = serial.readline().decode()
+            if "$GPGGA" in line:
+                parsed_data = pynmea2.parse(line)
+                # check for empty values
+                try:
+                    lat_degree = int(float(parsed_data.lat) / 100)
+                    lat_minute = float(parsed_data.lat) - lat_degree*100
+                    lon_degree = int(float(parsed_data.lon) / 100)
+                    lon_minute = float(parsed_data.lon) - lon_degree*100
+                    data_string = u'%s\u00b0%s\'%s,%s\u00b0%s\'%s,%s%s' % (lat_degree, lat_minute, parsed_data.lat_dir,
+                                                                           lon_degree, lon_minute, parsed_data.lon_dir,
+                                                                           parsed_data.altitude, parsed_data.altitude_units.lower())
 
-          self.send('gps', data_string)
-        except:
-          pass
+                    self.send('gps', data_string)
+                except:
+                    pass
+
 
 register = GPSPluign.register
 
 if __name__ == '__main__':
-  def callback(sensor, data):
-    print(sensor)
-    print(data)
-    print()
+    def callback(sensor, data):
+        print(sensor)
+        print(data)
+        print()
 
-  GPSPluign.run_standalone(callback)
+    GPSPluign.run_standalone(callback)
