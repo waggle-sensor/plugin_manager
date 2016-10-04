97e3641ddafb870a77eb2fdeaf5a7ef5269c506f
Peter Lane <lane@uchicago.edu>
2016-10-04 10:03:33 -0500

    waggle_image -> core. Removed some dependencies that are installed by core configure.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 
-apt-get install -y python-pip python-dev
-pip install tabulate
 pip install pyserial
 pip install crcmod
 pip install psutil
 
 # python3
 
-apt-get install -y python3-pip
-
 pip3 install crcmod
 pip3 install pyserial
-pip3 install tabulate
 pip3 install pyzmq
 pip3 install psutil
 pip3 install pyinotify
