32b5acab858ed8f009c451755eabc3dfd31d303a
Yongho Kim <ironyonghokim@gmail.com>
2017-12-12 19:09:33 -0600

    Working on coresense_4

--- plugins/coresense_4/plugin.py
+++ plugins/coresense_4/plugin.py
@@ -99,20 +99,64 @@ class DeviceHandler(object):
         self.serial.write(bytes(buffer))
         return self.read_response()
 
+def get_config_table():
+    sensor_config_file = '/wagglerw/waggle/sensor_table.conf'
+    sensor_table = None
+    if os.path.isfile(sensor_config_file):
+        with open(sensor_config_file) as config:
+            sensor_table = json.loads(config.read())
+    else:
+        sensor_table = {
+            'MetMAC': { 'sensor_id': 0x00, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'TMP112': { 'sensor_id': 0x01, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'HTU21D': { 'sensor_id': 0x02, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'HIH4030': { 'sensor_id': 0x03, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'BMP180': { 'sensor_id': 0x04, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'PR103J2': { 'sensor_id': 0x05, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'TSL250RDMS': { 'sensor_id': 0x06, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'MMA8452Q': { 'sensor_id': 0x07, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'SPV1840LR5H-B': { 'sensor_id': 0x08, 'function_call': 'sensor_read', 'interval': 1 },  #o 63 readings
+            'TSYS01': { 'sensor_id': 0x09, 'function_call': 'sensor_read', 'interval': 1 },  #o
+
+            'HMC5883L': { 'sensor_id': 0x0A, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'HIH6130': { 'sensor_id': 0x0B, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'APDS_9006_020': { 'sensor_id':0x0C, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'TSL260': { 'sensor_id': 0x0D, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'TSL250RDLS': { 'sensor_id': 0x0E, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'MLX75305': { 'sensor_id': 0x0F, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'ML8511': { 'sensor_id': 0x10, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
+            'TMP421': { 'sensor_id': 0x13, 'function_call': 'sensor_read', 'interval': 1 },  #o
+
+            # 'BusTMP112': { 'function_call': 'bus_read', 'bus_type': 0x00, 'bus_address': 0x48, 'params': [0x00], 'interval': 1 },
+            # 'BusHTU21D': { 'function_call': 'bus_read', 'bus_type': 0x00, 'bus_address': 0x40, 'params': [0xF3, 0xF5], 'interval': 1 },
+            # 'BusChemsense': { 'function_call': 'bus_read', 'bus_type': 0x02, 'bus_address': 0x03, 'params': [], 'interval': 1 },
+
+            # 'ChemConfig': { 'sensor_id': 0x16, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'Chemsense': { 'sensor_id': 0x2A, 'function_call': 'sensor_read', 'interval': 1 },  #o
+
+            # 'AlphaON': { 'sensor_id': 0x2B, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'AlphaFirmware': { 'sensor_id': 0x30, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'AlphaSerial': { 'sensor_id': 0x29, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            'AlphaHisto': { 'sensor_id': 0x28, 'function_call': 'sensor_read', 'interval': 1 },  #o
+            # 'AlphaConfig': { 'sensor_id': 0x31, 'function_call': 'sensor_read', 'interval': 1 },
+        }
+        with open(sensor_config_file, 'w') as config:
+            config.write(json.dumps(sensor_table))
+    return sensor_table
 
 
 class CoresensePlugin4(Plugin):
     plugin_name = 'coresense'
     plugin_version = '4'
 
-    def __init__(self, input_handler, sensors, hrf=False):
+    def __init__(self, input_handler, hrf=False):
         super().__init__()
-        self.sensors = sensors
+        self.sensors = get_config_table()
         for sensor in self.sensors:
             s = self.sensors[sensor]
             s['last_updated'] = time.time()
             self.sensors[sensor] = s
-        self.input_handler = input_handler
+        self.input_handler = DeviceHandler(device)
         self.hrf = hrf
 
         self.function_type = {
@@ -182,7 +226,6 @@ class CoresensePlugin4(Plugin):
 
     def run(self):
         while True:
-            # Blocking function
             requests = self._get_requests()
             if len(requests) > 0:
                 # print(requests)
@@ -192,70 +235,38 @@ class CoresensePlugin4(Plugin):
                     print('Errors or invalid crc')
                 else:
                     if self.hrf:
-                        self._print(message, hrf=self.hrf)
+                        self._print(message)
                     else:
                         self.send(sensor='frame', data=message)
             else:
-                time.sleep(0.5)
+                time.sleep(1)
+
+
+class CoresenseWorker4(object):
+    plugin_name = 'coresense'
+    plugin_version = '4'
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--input-sensor', action='store_true', help='Data from the sensor board')
     parser.add_argument('--hrf', action='store_true', help='Print in human readable form')
+    parser.add_argument('--input-beehive', action='store_true', help='Data from beehive')
     args = parser.parse_args()
 
-    sensor_config_file = '/wagglerw/waggle/sensor_table.conf'
-    if os.path.isfile(sensor_config_file):
-        with open(sensor_config_file) as config:
-            sensor_table = json.loads(config.read())
+    if args.input_sensor:
+        if not os.path.exists(device):
+            exit(1)
+
+        plugin = CoresensePlugin4(hrf=args.hrf)
+        try:
+            plugin.run()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            plugin.close()
+    elif args.input_beehive:
+        worker = CoresenseWorker4()
     else:
-        sensor_table = {
-            'MetMAC': { 'sensor_id': 0x00, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'TMP112': { 'sensor_id': 0x01, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'HTU21D': { 'sensor_id': 0x02, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'HIH4030': { 'sensor_id': 0x03, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'BMP180': { 'sensor_id': 0x04, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'PR103J2': { 'sensor_id': 0x05, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'TSL250RDMS': { 'sensor_id': 0x06, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'MMA8452Q': { 'sensor_id': 0x07, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'SPV1840LR5H-B': { 'sensor_id': 0x08, 'function_call': 'sensor_read', 'interval': 1 },  #o 63 readings
-            'TSYS01': { 'sensor_id': 0x09, 'function_call': 'sensor_read', 'interval': 1 },  #o
-
-            'HMC5883L': { 'sensor_id': 0x0A, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'HIH6130': { 'sensor_id': 0x0B, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'APDS_9006_020': { 'sensor_id':0x0C, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'TSL260': { 'sensor_id': 0x0D, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'TSL250RDLS': { 'sensor_id': 0x0E, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'MLX75305': { 'sensor_id': 0x0F, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'ML8511': { 'sensor_id': 0x10, 'function_call': 'sensor_read', 'interval': 1 },  #o, light, return raw
-            'TMP421': { 'sensor_id': 0x13, 'function_call': 'sensor_read', 'interval': 1 },  #o
-
-            # 'BusTMP112': { 'function_call': 'bus_read', 'bus_type': 0x00, 'bus_address': 0x48, 'params': [0x00], 'interval': 1 },
-            # 'BusHTU21D': { 'function_call': 'bus_read', 'bus_type': 0x00, 'bus_address': 0x40, 'params': [0xF3, 0xF5], 'interval': 1 },
-            # 'BusChemsense': { 'function_call': 'bus_read', 'bus_type': 0x02, 'bus_address': 0x03, 'params': [], 'interval': 1 },
-
-            # 'ChemConfig': { 'sensor_id': 0x16, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'Chemsense': { 'sensor_id': 0x2A, 'function_call': 'sensor_read', 'interval': 1 },  #o
-
-            # 'AlphaON': { 'sensor_id': 0x2B, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'AlphaFirmware': { 'sensor_id': 0x30, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'AlphaSerial': { 'sensor_id': 0x29, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            'AlphaHisto': { 'sensor_id': 0x28, 'function_call': 'sensor_read', 'interval': 1 },  #o
-            # 'AlphaConfig': { 'sensor_id': 0x31, 'function_call': 'sensor_read', 'interval': 1 },
-        }
-        with open(sensor_config_file, 'w') as config:
-            config.write(json.dumps(sensor_table))
-
-    handler = None
-    if os.path.exists(device):
-        handler = DeviceHandler(device)
-    else:
-        exit(1)
-
-    plugin = CoresensePlugin4(handler, sensors=sensor_table, hrf=args.hrf)
-    try:
-        plugin.run()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        plugin.close()
+        parser.
